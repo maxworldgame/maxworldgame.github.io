bbcef2848dae8074957bf4d1ecc25912ad37db5a
Max Smiley <maxsmiley0@gmail.com>
2019-05-18 22:08:32 -0700

    update

--- Animation.js
+++ Animation.js
@@ -1,29 +1,26 @@
-class Animation {
-  PImage[] images;
-  int imageCount, imageWidth, imageHeight;
-  float gifSpeed = 1;
-  float frame;
-  
-  Animation(String imagePrefix, int imageCount, int imageWidth, int imageHeight, float gifSpeed) {
-    this.gifSpeed = gifSpeed;
-    this.imageWidth = imageWidth;
-    this.imageHeight = imageHeight;
-    this.imageCount = imageCount;
-    images = new PImage[imageCount];
+function Animation(imagePrefix, imageCount, imageWidth, imageHeight, gifSpeed) {
+  this.imagePrefix = imagePrefix;
+  this.imageCount = imageCount;
+  this.imageWidth = imageWidth;
+  this.imageHeight = imageHeight;
+  this.gifSpeed = gifSpeed;
 
-    for (int i = 0; i < imageCount; i++) {
-      // Use nf() to number format 'i' into two digits
-      String filename = "images/" + imagePrefix + "/" + imagePrefix + nf(i, 2) + ".png";
-      images[i] = loadImage(filename);
-    }
+  let images = [];
+  var frame = 0;
+  
+  
+  for (var i = 0; i < imageCount; i++) {
+    // Use nf() to number format 'i' into two digits
+    var filename = "data/images/" + imagePrefix + "/" + imagePrefix + nf(i, 2, 0) + ".png";
+    images[i] = loadImage(filename);
   }
 
-  void display(float xpos, float ypos) {
+  this.display = function(xpos, ypos) {
     frame = (frame+(gifSpeed/10.0)) % imageCount;
     image(images[floor(frame)], xpos, ypos, imageWidth, imageHeight);
   }
   
-  int getWidth() {
+  this.getWidth = function() {
     return images[0].width;
   }
 }
--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 let img;
 let song;
 let player;
+let marioRight;
 
 function setup() {
 	createCanvas(1250, 750);
 	img = loadImage('player.png');
 	song = loadSound('data/sound/fightSong.mp3');
 	player = new Player(100, 100, 3);
+	marioRight = new Animation("marioRight", 8, 300, 300, 3);
 	imageMode(CENTER);
+	song.play();
 }
 
 var playerSize = 100;
@@ -15,12 +18,10 @@ var playerSize = 100;
 function draw() {
 	background(100);
 
-	fill(255, 0, 0);
-	image(img, player.getPlayerX(), player.getPlayerY(), playerSize, playerSize);
+	marioRight.display(player.getPlayerX(), player.getPlayerY());
 
 	if(mouseIsPressed){
-		playerSize++;
 		player.changePlayerX(1);
-		song.play();
+		
 	}
 }
