d29b2b3347560208e15ac4412f405d094ec8a7c1
Max Smiley <maxsmiley0@gmail.com>
2019-05-18 21:38:07 -0700

    update

--- Player.js
+++ Player.js
@@ -1,105 +1,39 @@
 class Player {
-  int playerX;
-  int playerY;
-  int playerSpeed;
-  int playerDamage;
+  var playerX;
+  var playerY;
+  var playerSpeed;
   
-  float health;
-  int maxHealth;
-  float healthPercentage;
-  
-  float mana;
-  int maxMana;
-  float manaPercentage;
-  
-  boolean isStunned = false;
-  
-  Player(int maxHealth, int maxMana, int playerX, int playerY, int playerSpeed, int playerDamage) {
-    this.maxHealth = maxHealth;
-    health = saveDataInt[6];
-    this.maxMana = maxMana;
-    mana = saveDataInt[8];
-    
-    healthPercentage = health/maxHealth;
-    manaPercentage = mana/maxMana;
-    
+  Player(var playerX, var playerY, var playerSpeed) {
     this.playerX = playerX;
     this.playerY = playerY;
     this.playerSpeed = playerSpeed;
-    this.playerDamage = playerDamage;
   }
   
-  void healthBar(int x, int y){
-    healthPercentage = health/maxHealth;
-    strokeWeight(1);
-    fill(0);
-    rect(x - 15, y - 65, 120, 15);
-    fill(255, 0, 0);
-    rect(x - 75 + 60*healthPercentage, y - 65, 120*healthPercentage, 15);
-    
-    manaPercentage = mana/maxMana;
-    strokeWeight(1);
-    fill(0);
-    rect(x - 15, y - 51, 120, 5);
-    fill(0, 0, 255);
-    rect(x - 75 + 60*manaPercentage, y - 51, 120*manaPercentage, 5);
-  }
-  
-  void spawn(int x, int y){
-    marioRight.display(x, y);
-  }
-  
-  void takeDamage(int amount){
-    timer = 0;
-    playerDamageText = true;
-    tempDamageVar = amount;
-    hit.rewind();
-    hit.play();
-    health -= amount;
-    nextTurn();
-  }
-  
-  int getPlayerX(){
+  getPlayerX(){
     return playerX;
   }
   
-  int getPlayerY(){
+  getPlayerY(){
     return playerY;
   }
   
-  void setPlayerX(int x){
+  setPlayerX(var x){
     playerX = x;
   }
   
-  void setPlayerY(int y){
+  setPlayerY(var y){
     playerY = y;
   }
   
-  void changePlayerX(int c){
+  changePlayerX(var c){
     playerX += c;
   }
   
-  void changePlayerY(int c){
+  changePlayerY(var c){
     playerY += c;
   }
   
-  int getPlayerSpeed(){
+  getPlayerSpeed(){
     return playerSpeed;
-  }
-  
-  void spendMana(int amount){
-    mana -= amount;
-  }
-  
-  boolean isStunned(){
-    return isStunned;
-  }
-  
-  void setStun(boolean b){
-    isStunned = b;
-  }
-  
-  int getPlayerDamage(){
-    return playerDamage;
   }
 }
--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 let img;
 let song;
+let player;
 
 function setup() {
 	createCanvas(1250, 750);
 	img = loadImage('player.png');
 	song = loadSound('data/sound/fightSong.mp3');
+	player = new Player(100, 100, 3);
 	imageMode(CENTER);
 }
 
@@ -14,10 +16,11 @@ function draw() {
 	background(100);
 
 	fill(255, 0, 0);
-	image(img, mouseX, mouseY, playerSize, playerSize);
+	image(img, player.playerX, player.playerY, playerSize, playerSize);
 
 	if(mouseIsPressed){
 		playerSize++;
+		player.changePlayerX(1);
 		song.play();
 	}
 }
