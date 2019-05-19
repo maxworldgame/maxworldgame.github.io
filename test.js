1d2accb53d1ad3b7372aea71e0297e519fcdb53a
Max Smiley <maxsmiley0@gmail.com>
2019-05-18 22:17:36 -0700

    up

--- test.js
+++ test.js
@@ -3,23 +3,23 @@ let song;
 let player;
 let marioRight;
 
-function preload(){
-	song = loadSound('data/sound/fightSong.mp3');
-}
-
 function setup() {
 	createCanvas(1250, 750);
 	img = loadImage('player.png');
 	player = new Player(100, 100, 3);
+	song = loadSound('data/sound/fightSong.mp3');
 	marioRight = new Animation("marioRight", 8, 300, 300, 3);
 	imageMode(CENTER);
-	song.play();
 }
 
 var playerSize = 100;
-
+	
 function draw() {
 	background(100);
+
+	if (keyIsPressed){
+	song.play();
+	}
 
 	marioRight.display(player.getPlayerX(), player.getPlayerY());
 
